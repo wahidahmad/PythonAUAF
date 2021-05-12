38cfa946576d8ceb216ba318554d0fed6f0869a4
Wahid Ahmad Shahid <wahid.ahmad@gmail.com>
2021-05-13 02:41:32 +0430

    Home Work
    Using list and Test

---
+++ homework.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+# using input - and concatnation
+
+first_name = input("Please Enter Your First Name ")
+last_name = input('Please Enter your last Name')
+age = input(" Please Enter your Age :")
+print(" your First Name is  " +first_name + ' and your Last Name is  ' +last_name +  ' Your '  +age + ' Years old')
+
+
+# # List 
+
+# In[25]:
+
+
+my_friend_list = ['Abdubaker','Ahmad Bilal', 'Mohammad Usman','Umar' , 'Ali', 'Akif' , 'Umran']
+print(my_friend_list)
+
+
+# In[26]:
+
+
+my_friend_list.sort()
+print (my_friend_list)
+
+
+# In[29]:
+
+
+bestfriendlist = my_friend_list.copy()
+print(bestfriendlist)
+
+
+# In[ ]:
+
+
+
+
